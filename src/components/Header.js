eb920791db667c5938fa6ae538925f0e45d0ca00
James Mwendwa <mwendwajames32@gmail.com>
2022-11-08 18:38:17 +0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { TfiVideoClapper } from "react-icons/tfi";
+import { TfiWorld } from "react-icons/tfi";
+import { SiImdb } from "react-icons/si";
+import { MdOutlineOndemandVideo } from "react-icons/md";
+import { IoCaretForwardCircleOutline } from "react-icons/io5";
 
 function Header() {
   return (
-    <div className="header">
-      <Link to="/">
-        <h2>Streamer</h2>
+    <div className="conatiner mx-auto relative flex justify-around bg-slate-500 p-5 text-lg">
+      <Link className="flex items-center cursor-pointer" to="/">
+        <span>:::</span> <h2 className="ml-2 text-2xl">Streamer</h2>
       </Link>
 
-      <div className="headerLinks">
-        <Link to="/">Home</Link>
-        <Link to="/genre">Genre</Link>
-        <Link to="/country">Country</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/tvShows">Tv Shows</Link>
-        <Link to="/topIMDB">Top IMDB</Link>
+      <div className="flex">
+        <div className="flex items-center flex-col">
+          <AiOutlineHome />
+          <Link className="text-lg" to="/">
+            Home
+          </Link>
+        </div>
+        <div className="flex items-center flex-col justify-between">
+          <IoCaretForwardCircleOutline />
+          <Link className="ml-5 text-lg" to="/genre">
+            Genre
+          </Link>
+        </div>
+        <div className="flex items-center flex-col justify-between">
+          <TfiWorld />
+          <Link className="ml-5 text-lg" to="/country">
+            Country
+          </Link>
+        </div>
+        <div className="flex items-center flex-col justify-between">
+          <TfiVideoClapper />
+          <Link className="ml-5 text-lg" to="/movies">
+            Movies
+          </Link>
+        </div>
+        <div className="flex items-center flex-col justify-between">
+          <MdOutlineOndemandVideo />
+          <Link className="ml-5 text-lg" to="/tvShows">
+            Tv Series
+          </Link>
+        </div>
+        <div className="flex items-center flex-col justify-between">
+          <SiImdb />
+          <Link className="ml-5 text-lg" to="/topIMDB">
+            Top IMDB
+          </Link>
+        </div>
       </div>
     </div>
   );
